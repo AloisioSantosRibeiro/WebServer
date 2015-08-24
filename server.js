4823f052ac11bbbf161900c167b44574d0394190
Aloisio Ribeiro <aloisio@mobstats.com>
2015-08-24 14:42:44 +0100

    adding Cookie-Parser and Express-Session

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var morgan = require('morgan');
+var express      = require('express');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
 
 app.use( morgan('dev') );
+app.use( cookieParser() );
+app.use( session({secret: 'anyStringOfText', saveUninitialized: true, resave: true}) );
+
+
 app.use('/', function(req, res) {
     res.send('Our First Express-program!');
+    console.log('\n');
+    console.log('================================================================================');
+    console.log(req.cookies);
+    console.log('================================================================================');
+    console.log(req.session);
+    console.log('\n');
 });
 
 app.listen(port);
