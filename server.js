c8edb96c8a7a91039e6020964ea31e016e9c152f
Aloisio Ribeiro <aloisio@mobstats.com>
2015-08-24 16:12:15 +0100

    adding Mongoose module and separating routes in a new file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express      = require('express');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
+var mongoose     = require('mongoose');
+
+var configDB     = require('./config/database.js');
+mongoose.connect(configDB.url);
 
 var app = express();
 
@@ -12,15 +16,20 @@ app.use( cookieParser() );
 app.use( session({secret: 'anyStringOfText', saveUninitialized: true, resave: true}) );
 
 
-app.use('/', function(req, res) {
-    res.send('Our First Express-program!');
-    console.log('\n');
-    console.log('================================================================================');
-    console.log(req.cookies);
-    console.log('================================================================================');
-    console.log(req.session);
-    console.log('\n');
-});
+// REPLACED
+//
+// app.use('/', function(req, res) {
+//     res.send('Our First Express-program!');
+//     console.log('\n');
+//     console.log('================================================================================');
+//     console.log(req.cookies);
+//     console.log('================================================================================');
+//     console.log(req.session);
+//     console.log('\n');
+// });
+//
+// BY...
+require('./app/routes.js')(app);
 
 app.listen(port);
 console.log('Server running on port: ' + port);
